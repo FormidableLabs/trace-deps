ff747811d1bf67d8b8907bead71577d84e23c74c
Ryan Roemer <ryan.roemer@formidable.com>
2020-02-28 11:12:01 -0800

    Start more tests

--- test/lib/trace.spec.js
+++ test/lib/trace.spec.js
@@ -1,9 +1,13 @@
 "use strict";
 
+const path = require("path");
 const mock = require("mock-fs");
 
 const { traceFile, traceFiles } = require("../../lib/trace");
 
+const INDENT = 2;
+const stringify = (val) => JSON.stringify(val, null, INDENT);
+const fullPath = (paths) => paths.map((p) => path.resolve(p));
 
 describe("lib/trace", () => {
   beforeEach(() => {
@@ -23,11 +27,38 @@ describe("lib/trace", () => {
       expect(await traceFile({ srcPath: "hi.js" })).to.eql([]);
     });
 
-    it("handles circular dependencies"); // TODO
-    it("handles single requires with .js"); // TODO
+    it("handles single requires with .js", async () => {
+      mock({
+        "hi.js": `
+          const one = require("one");
+          require("two");
+        `,
+        node_modules: {
+          one: {
+            "package.json": stringify({
+              main: "index.js"
+            }),
+            "index.js": "module.exports = 'one';"
+          },
+          two: {
+            "package.json": stringify({
+              main: "index.js"
+            }),
+            "index.js": "module.exports = 'two';"
+          }
+        }
+      });
+
+      expect(await traceFile({ srcPath: "hi.js" })).to.eql(fullPath([
+        "node_modules/one/index.js",
+        "node_modules/two/index.js"
+      ]));
+    });
+
     it("handles single imports with .mjs"); // TODO
     it("handles nested requires with .js"); // TODO
     it("handles nested imports with .mjs"); // TODO
+    it("handles circular dependencies"); // TODO
     it("ignores specified names and prefixes"); // TODO
   });
 
