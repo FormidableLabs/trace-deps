78a2119fc74d3221ba9d74ad5f1965a698d8b116
Ryan Roemer <ryan.roemer@formidable.com>
2020-03-26 15:14:33 -0700

    Add getLastPackage

--- test/lib/package.spec.js
+++ test/lib/package.spec.js
@@ -2,13 +2,12 @@
 
 const { normalize } = require("path");
 
-const { getPackages } = require("../../lib/package");
+const { getPackages, getLastPackage } = require("../../lib/package");
 
 describe("lib/package", () => {
   describe("getPackages", () => {
     it("handles empty input", () => {
       expect(getPackages()).to.eql([]);
-      expect(getPackages([])).to.eql([]);
       expect(getPackages(null)).to.eql([]);
     });
 
@@ -31,6 +30,35 @@ describe("lib/package", () => {
         "path/to/node_modules/@scope/foo/node_modules/three/node_modules/four/node_modules"
       )))
         .to.eql(["@scope/foo", "three", "four"]);
+    });
+  });
+
+  describe("getLastPackage", () => {
+    it("handles empty input", () => {
+      expect(getLastPackage()).to.eql(null);
+      expect(getLastPackage(null)).to.eql(null);
+    });
+
+    it("handles no modules", () => {
+      expect(getLastPackage(normalize(""))).to.eql(null);
+      expect(getLastPackage(normalize("src"))).to.eql(null);
+      expect(getLastPackage(normalize("path/to/node_modules"))).to.eql(null);
+      expect(getLastPackage(normalize("path/to/node_modules/@scope"))).to.eql(null);
+    });
+
+
+    it("handles single modules", () => {
+      expect(getLastPackage(normalize("node_modules/bar"))).to.eql("bar");
+      expect(getLastPackage(normalize("path/to/node_modules/@scope/foo"))).to.eql("@scope/foo");
+    });
+
+    it("handles nested modules", () => {
+      expect(getLastPackage(normalize("node_modules/bar/node_modules/@scope/nested-bar")))
+        .to.eql("@scope/nested-bar");
+      expect(getLastPackage(normalize(
+        "path/to/node_modules/@scope/foo/node_modules/three/node_modules/four/node_modules"
+      )))
+        .to.eql("four");
     });
   });
 });
