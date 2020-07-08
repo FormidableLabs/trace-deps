f75ddc9318dbc8bc5afd4d31abd1b3041efcb008
Ryan Roemer <ryan.roemer@formidable.com>
2020-07-08 16:17:31 -0700

    Start CLI

---
+++ bin/trace-deps.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const pkg = require("../package.json");
+const { traceFile } = require("../lib/trace");
+const { log, error } = console;
+
+const DEFAULT_OUTPUT = "text";
+
+const USAGE = `
+Usage: ${pkg.name} <action> [options]
+
+Actions: (<action>)
+  trace                     Trace dependencies and misses for a file
+
+Options:
+  --input, -i   (trace)     Starting file to trace              [string]
+  --output, -o  (trace)     Output format (text, json)          [string] [default: text]
+  --help, -h                Show help                           [boolean]
+  --version, -v             Show version number                 [boolean]
+
+Examples:
+  ${pkg.name} trace --input ./path/to/file.js     Trace a source file
+`.trim();
+
+// ============================================================================
+// Actions
+// ============================================================================
+const help = async () => { log(USAGE); };
+const version = async () => { log(pkg.version); };
+const trace = async ({ input, output }) => {
+  if (!input) {
+    throw new Error("Must specify --input file to trace");
+  }
+
+  const data = await traceFile({
+    srcPath: input,
+    bailOnMissing: false
+  });
+
+  log(JSON.stringify(data, null, 2));
+};
+
+// ============================================================================
+// Configuration
+// ============================================================================
+// Get action or help / version name
+const getAction = (args, opts) => {
+  // Return actions in priority order.
+  if (args.includes("--help") || args.includes("-h")) { return help; }
+  if (args.includes("--version") || args.includes("-v")) { return version; }
+  if (args.includes("trace")) { return trace; }
+
+  // Default.
+  return help;
+};
+
+// Get options for actions.
+const getOptions = (args) => ({
+  input: args.find((val, i) => ["--input", "-i"].includes(args[i - 1])) || null,
+  output: args.find((val, i) => ["--output", "-o"].includes(args[i - 1])) || DEFAULT_OUTPUT
+});
+
+// ============================================================================
+// Script
+// ============================================================================
+const main = async () => {
+  const args = process.argv.slice(2); // eslint-disable-line no-magic-numbers
+  const opts = getOptions(args);
+  const action = getAction(args, opts);
+  const actionName = JSON.stringify({ action: action.name });
+  log(`Starting ${actionName} with options ${JSON.stringify(opts)}`); // TODO: REMOVE (?)
+  await action(opts);
+};
+
+if (require.main === module) {
+  main().catch((err) => {
+    error(err);
+    process.exit(1); // eslint-disable-line no-process-exit
+  });
+}
